441ef1ca93083100e4163ba7a2c88c95cc0c31d2
dav1dvorobev <dav1dvorobev@vk.com>
2024-12-19 20:36:46 +0300

    added experiment in 10 generated cases

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,41 @@ mod graph;
 
 use anyhow::Result;
 use graph::Graph;
-use std::{collections::VecDeque, fs::OpenOptions, io::Write, time::Instant};
+use std::collections::VecDeque;
+use tokio::{fs::OpenOptions, io::AsyncWriteExt, time::Instant};
 
-fn main() -> Result<()> {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open("output.csv")?;
-    file.write_all(b"v,best,medium,worst\n")?;
-    let mut buf = [0; 3];
-    for v in 10..1_000 + 1 {
-        let edges_quantities = [v, v * (v - 1) / 4, v * (v - 1) / 2];
-        for i in 0..edges_quantities.len() {
-            let graph = Graph::rand(v, edges_quantities[i]);
-            let start_timestamp = Instant::now();
-            let _ = bfs(&graph, 0);
-            buf[i] = (Instant::now() - start_timestamp).as_nanos();
-        }
-        file.write_all(format!("{},{},{},{}\n", v, buf[0], buf[1], buf[2]).as_bytes())?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    tokio::fs::create_dir_all("output/").await?;
+    let mut tasks = vec![];
+    for i in 1..=10 {
+        let task = tokio::spawn(async move {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(format!("output/data-{i}.csv"))
+                .await
+                .expect("failed to open");
+            file.write_all(b"v,best,medium,worst\n")
+                .await
+                .expect("failed to write");
+            let mut buf = [0; 3];
+            for v in 10..=1000 {
+                let edges_quantities = [v, v * (v - 1) / 4, v * (v - 1) / 2];
+                for i in 0..edges_quantities.len() {
+                    let graph = Graph::rand(v, edges_quantities[i]);
+                    let start_timestamp = Instant::now();
+                    let _ = bfs(&graph, 0);
+                    buf[i] = (Instant::now() - start_timestamp).as_nanos();
+                }
+                file.write_all(format!("{},{},{},{}\n", v, buf[0], buf[1], buf[2]).as_bytes())
+                    .await
+                    .expect("failed to write");
+            }
+        });
+        tasks.push(task);
     }
+    for task in tasks { task.await?; }
     Ok(())
 }
 
